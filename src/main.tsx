687311e238ece46913563fc7687895dc658ceb37
Kombuccino <contact@bioferment.fr>
2026-09-02 09:35:36 +0200

    style: load landscape core overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './core/coreIcons.css'
 import './core/actionDock.css'
+import './core/orientation.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

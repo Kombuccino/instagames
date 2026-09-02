5cab3fe726b7c22d3df800e0c054273924bc1c2f
Kombuccino <contact@bioferment.fr>
2026-09-02 14:34:36 +0200

    style: load comment player avatars

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './core/actionDock.css'
 import './core/orientation.css'
 import './core/gameLayout.css'
 import './core/comments.css'
+import './core/commentPlayerIcons.css'
 import './core/profileLeaderboard.css'
 import './core/desktopDock.css'
 

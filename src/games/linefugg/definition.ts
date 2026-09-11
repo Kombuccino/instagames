dda64f24f13c3ea7a74795f395bae22bb53045d4
Kombuccino <contact@bioferment.fr>
2026-09-11 20:01:49 +0200

    release(linefugg): bump optimized runtime to 0.5.2

--- src/games/linefugg/definition.ts
+++ src/games/linefugg/definition.ts
@@ -22,8 +22,8 @@ export const gameDefinition: InstagameDefinition = {
   welcome: LINEFUGG_WELCOME,
   component: LineFugg,
   release: {
-    version: '0.5.1',
-    updatedAt: '2026-09-10T10:23:00+02:00',
+    version: '0.5.2',
+    updatedAt: '2026-09-11T20:01:00+02:00',
     changelogPath: 'src/games/linefugg/CHANGELOG.md',
   },
   instructions: {

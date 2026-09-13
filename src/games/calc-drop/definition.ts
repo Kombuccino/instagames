dedb53be750729b5ac232c19117eb28683115a7d
Kombuccino <contact@bioferment.fr>
2026-09-13 23:35:31 +0200

    chore(tetramindfck): bump gameplay art release

--- src/games/calc-drop/definition.ts
+++ src/games/calc-drop/definition.ts
@@ -22,8 +22,8 @@ export const gameDefinition: InstagameDefinition = {
   welcome: TETRAMINDFCK_WELCOME,
   component: TetraMindFck,
   release: {
-    version: '0.8.0',
-    updatedAt: '2026-09-13T10:48:00+02:00',
+    version: '0.8.1',
+    updatedAt: '2026-09-13T23:31:00+02:00',
     changelogPath: 'src/games/calc-drop/CHANGELOG.md',
   },
   instructions: {
